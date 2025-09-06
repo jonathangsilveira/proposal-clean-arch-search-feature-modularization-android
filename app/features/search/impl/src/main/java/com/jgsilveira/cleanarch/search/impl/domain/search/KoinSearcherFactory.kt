4d0990a420181da4c183bc05f2d9f46cd2ab4316
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-09-06 15:44:49 -0300

    Use context config factory

--- app/features/search/impl/src/main/java/com/jgsilveira/cleanarch/search/impl/domain/search/KoinSearcherFactory.kt
+++ app/features/search/impl/src/main/java/com/jgsilveira/cleanarch/search/impl/domain/search/KoinSearcherFactory.kt
@@ -1,19 +1,19 @@
 package com.jgsilveira.cleanarch.search.impl.domain.search
 
-import com.jgsilveira.cleanarch.search.impl.domain.mapper.SearchContextConfigMapper
+import com.jgsilveira.cleanarch.search.factory.SearcherFactory
+import com.jgsilveira.cleanarch.search.impl.domain.mapper.SearchContextConfigFactory
 import com.jgsilveira.cleanarch.search.model.SearchContext
 import com.jgsilveira.cleanarch.search.searchable.Searchable
-import com.jgsilveira.cleanarch.search.factory.SearcherFactory
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import org.koin.core.parameter.parametersOf
 
 internal class KoinSearcherFactory(
-    private val contextConfigMapper: SearchContextConfigMapper
+    private val contextConfigFactory: SearchContextConfigFactory
 ): SearcherFactory, KoinComponent {
 
     override fun createSearcher(context: SearchContext): Searchable {
-        val contextConfig = contextConfigMapper.fromContext(context)
+        val contextConfig = contextConfigFactory.fromContext(context)
         return get { parametersOf(contextConfig) }
     }
 }
