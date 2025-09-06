dfa055b716f82dd2f14aa28baf5352d18926774b
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2025-09-06 15:15:46 -0300

    Organize enum options

--- app/features/search/impl/src/main/java/com/jgsilveira/cleanarch/search/impl/domain/model/config/SearchGroup.kt
+++ app/features/search/impl/src/main/java/com/jgsilveira/cleanarch/search/impl/domain/model/config/SearchGroup.kt
@@ -1,5 +1,5 @@
 package com.jgsilveira.cleanarch.search.impl.domain.model.config
 
 enum class SearchGroup {
-    GENERAL, BENEFITS_P2P
+    GENERAL, PEOPLE
 }
--- app/features/search/public/src/main/java/com/jgsilveira/cleanarch/search/model/SearchResultSectionDisplay.kt
+++ app/features/search/public/src/main/java/com/jgsilveira/cleanarch/search/model/SearchResultSectionDisplay.kt
@@ -1,5 +1,5 @@
 package com.jgsilveira.cleanarch.search.model
 
 enum class SearchResultSectionDisplay {
-    LIST, FEEDBACK, CAROUSEL, REMINDER
+    LIST, FEEDBACK, CAROUSEL
 }
